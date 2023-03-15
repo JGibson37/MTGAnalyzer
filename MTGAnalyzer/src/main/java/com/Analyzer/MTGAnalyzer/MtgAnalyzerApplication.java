17df76779a80a6da7b37cb7e1fc31ced81712a74
Jason Gibson <65173587+JGibson37@users.noreply.github.com>
2023-03-15 18:43:11 -0400

    Add files via upload

---
+++ MTGAnalyzer/src/main/java/com/Analyzer/MTGAnalyzer/MtgAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.Analyzer.MTGAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgAnalyzerApplication.class, args);
+	}
+
+}
---
+++ MTGAnalyzer/src/test/java/com/Analyzer/MTGAnalyzer/MtgAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Analyzer.MTGAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
